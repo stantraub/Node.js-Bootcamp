332917bd5f35a85dfa55675ee29dd382348d3826
Stanley <stanley.traub@gmail.com>
2020-05-08 21:56:46 -0700

    Finished JWT setup

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({ 
+    name: { 
+        type: String, 
+        required: [true, 'You must enter a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'An email address is required'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email address']
+    },
+    photo: String, 
+    password: {
+        type: String,
+        required: [true, 'A password is required'],
+        minLength: 8,
+        select: false //Prevent password info from showing up in output
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'You must confirm your password'],
+        validator: function(el) {
+            // This only works on CREATE and SAVE!!!
+            return el === this.password //abc === abc
+        },
+        message: 'Passwords are not the same'
+    },
+    passwordChangedAt: Date
+})
+
+userSchema.pre('save',async function(next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified('password')) return next()
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12)
+
+    // Delete the password confirm field
+    this.passwordConfirm = undefined
+    next()
+})
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+   return await bcrypt.compare(candidatePassword, userPassword)
+}
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if (this.candidatePassword) {
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000)
+        return JWTTimestamp < changedTimestamp
+    }
+
+    // False means NOT changed
+    return false
+}
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
+
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
+
+
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE
@@ -16,6 +25,14 @@ mongoose.connect(DB, {
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message)
+  console.log('UNHANDLED REJECTION Shutting down...')
+  server.close(() => {
+    process.exit(1);
+  })
+})
