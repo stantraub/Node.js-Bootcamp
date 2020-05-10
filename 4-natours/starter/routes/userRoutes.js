9cb1e7ab5366b5f2f43eff466ccd1cf77e78e866
Stanley <stanley.traub@gmail.com>
2020-05-10 12:44:22 -0700

    Update user functionality

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -10,6 +10,10 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword', authController.resetPassword);
 
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword)
+
+router.patch('/updateMe', authController.protect, userController.updateMe)
+
 router
     .route('/')
     .get(userController.getAllUsers)
